886a6f9e0f6f1e676d2de4132f18b8d0d074df39
Grace Amole <gracey1235@gmail.com>
2025-06-24 17:01:43 +0200

    Delete app/sqlquery.py

--- app/sqlquery.py
+++
@@ -1,8 +0,0 @@
-# Function to extract SQL query from response
-import re
-def extract_sql_from_response(response):
-    queries = re.findall(r"SELECT.*?;", response, re.IGNORECASE | re.DOTALL)
-    if queries:
-        return queries[0].strip().rstrip(';')
-    else:
-        raise ValueError("No valid SQL query found.")
